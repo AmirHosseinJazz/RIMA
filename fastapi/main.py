d134fb3533a370a2cf36f474e83a3421e091ead8
madixiu <mahdi.moradi72@gmail.com>
2024-06-06 22:26:50 +0330

    fixed cores problem

--- fastapi/main.py
+++ fastapi/main.py
@@ -4,10 +4,27 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 import time
-
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+# ... other imports and code ...
+
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 class Asset(BaseModel):
